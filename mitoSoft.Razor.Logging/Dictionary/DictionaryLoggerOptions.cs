75ad27fe6c1e2b3d64200a67834ab47aed9ff93a
Michael <michael.mr.roth@live.com>
2021-11-15 00:29:08 +0100

    cleanup

--- mitoSoft.Razor.Logging/Dictionary/DictionaryLoggerOptions.cs
+++ mitoSoft.Razor.Logging/Dictionary/DictionaryLoggerOptions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace mitoSoft.Razor.Logging.Dictionary
+﻿namespace mitoSoft.Razor.Logging.Dictionary
 {
     public class DictionaryLoggerOptions : LoggerOptions<DictionaryLogger>
     {
