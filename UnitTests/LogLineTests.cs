cedfc67a8e83d3f2fcfb65e03880a392490bb6a8
Michael <michael.mr.roth@live.com>
2021-11-15 22:03:44 +0100

    improvments in LogLine formatter

---
+++ UnitTests/LogLineTests.cs
@@ -0,0 +1,71 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mitoSoft.Razor.Logging;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LogLineTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var logLine = new LogLine(new DateTime(1982, 3, 7, 6, 0, 0, DateTimeKind.Utc), LogLevel.Warning, "TestMessage", "Category1");
+
+            var s = logLine.ToString("[{date:yyyyMMdd HH:mm:ssZ}] [{level}] {message}");
+
+            Assert.AreEqual("[19820307 06:00:00Z] [warn] TestMessage", s);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var logLine = new LogLine(new DateTime(1982, 3, 7, 6, 0, 0, DateTimeKind.Utc), LogLevel.Warning, "TestMessage", "Category1");
+
+            var s = logLine.ToString("[{Date:yyyyMMdd HH:mm:ssZ}] [{level}] {message}");
+
+            Assert.AreEqual("[19820307 06:00:00Z] [warn] TestMessage", s);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var logLine = new LogLine(new DateTime(1982, 3, 7, 6, 0, 0, DateTimeKind.Utc), LogLevel.Warning, "TestMessage", "Category1");
+
+            var s = logLine.ToString("[{   Date :   yyyyMMdd HH:mm:ss}] [{level}] {message}");
+
+            Assert.AreEqual("[19820307 06:00:00] [warn] TestMessage", s);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var logLine = new LogLine(new DateTime(1982, 3, 7, 6, 0, 0, DateTimeKind.Utc), LogLevel.Warning, "TestMessage", "Category1");
+
+            var s = logLine.ToString("[{   Date :   yyyyMMdd} {date:HH:mm:ss fffZ}] [{level}] {message}");
+
+            Assert.AreEqual("[19820307 06:00:00 000Z] [warn] TestMessage", s);
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            var logLine = new LogLine(new DateTime(1982, 3, 7, 6, 0, 0, DateTimeKind.Utc), LogLevel.Warning, "TestMessage", "Category1");
+
+            var s = logLine.ToString("[{date:dd.MM.yyyy HH:mm:ss}] [{level}] {message} {MESSAge}");
+
+            Assert.AreEqual("[07.03.1982 06:00:00] [warn] TestMessage TestMessage", s);
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            var logLine = new LogLine(new DateTime(1982, 3, 7, 6, 0, 0, DateTimeKind.Utc), LogLevel.Warning, "TestMessage", "Category1");
+
+            var s = logLine.ToString("[{ date  }] [{level}] {message}");
+
+            Assert.AreEqual("[1982-03-07 06:00:00 000Z] [warn] TestMessage", s);
+        }
+    }
+}
--- mitoSoft.Razor.Logging/LogLine.cs
+++ mitoSoft.Razor.Logging/LogLine.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using mitoSoft.Razor.Logging.Extensions;
 using System;
+using System.Text.RegularExpressions;
 
 namespace mitoSoft.Razor.Logging
 {
@@ -24,15 +25,47 @@ namespace mitoSoft.Razor.Logging
 
         public string ToString(string format)
         {
-            var timestamp = this.Timestamp.ToFormattedString();
+            var logRecord = format;
 
-            var logRecord = format.ToLower();
-            logRecord = logRecord.Replace("{date}", timestamp);
-            logRecord = logRecord.Replace("{level}", this.LogLevel.ToShortString());
-            logRecord = logRecord.Replace("{loglevel}", this.LogLevel.ToShortString());
-            logRecord = logRecord.Replace("{category}", this.Category);
-            logRecord = logRecord.Replace("{categoryname}", this.Category);
-            logRecord = logRecord.Replace("{message}", this.Message);
+            var pattern = @"\{(.*?)\}";
+            var matches = Regex.Matches(logRecord, pattern);
+
+            foreach (Match match in matches)
+            {
+                var value = match.Value;
+                value = value.Trim('{', '}');
+                value = value.Replace(" ", "");
+                if (value.ToLower().StartsWith("date"))
+                {
+                    string dateString;
+                    if (value.Split(':').Length > 1)
+                    {
+                        var dateFormat = match.Value.Substring(match.Value.IndexOf(':')).TrimStart(':').Trim().Trim('{', '}');
+
+                        dateString = this.Timestamp.ToString(dateFormat);
+                    }
+                    else
+                    {
+                        dateString = this.Timestamp.ToFormattedString();
+                    }
+
+                    logRecord = logRecord.Replace(match.Value, dateString);
+                }
+                else if (value.ToLower() == "level"
+                      || value.ToLower() == "logevel")
+                {
+                    logRecord = logRecord.Replace(match.Value, this.LogLevel.ToShortString());
+                }
+                else if (value.ToLower() == "message")
+                {
+                    logRecord = logRecord.Replace(match.Value, this.Message);
+                }
+                else if (value.ToLower() == "category"
+                     || value.ToLower() == "categoryname")
+                {
+                    logRecord = logRecord.Replace(match.Value, this.Category);
+                }
+            }
 
             return logRecord;
         }
